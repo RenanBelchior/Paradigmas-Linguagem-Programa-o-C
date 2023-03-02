2776f4f386b3651dc28be5a756017225dfcd51de
Renan S Belchior <102825017+RenanBelchior@users.noreply.github.com>
2023-03-02 20:01:09 -0300

    Add files via upload

---
+++ Linguagem C aula 1/main4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *a;
+    a = malloc(sizeof(int));
+    
+    printf("-> %p", a);
+    printf("n\-> %ld", sizeof(int));
+    free(a);
+    return 0; 
+}
